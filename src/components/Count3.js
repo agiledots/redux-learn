c9aebff6d0b868d17ecefefe263007c7837a7bee
agiledots <agiledots@gmail.com>
2022-09-23 11:40:05 +0800

    feat: useDispatchを利用した。

--- src/components/Count3.js
+++ src/components/Count3.js
@@ -1,9 +1,18 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 // Componentsは useSelector Hooks で redux の state を取得
 
 function Count3() {
+  const dispatch = useDispatch();
+
+  const increase = () => {
+    dispatch({ type: "INCREASE_COUNT" });
+  };
+  const decrease = () => {
+    dispatch({ type: "DECREASE_COUNT" });
+  };
+
   const count = useSelector((state) => state.countReducer.count);
   const posts = useSelector((state) => state.postsReducer.posts);
 
@@ -12,6 +21,8 @@ function Count3() {
       <div className="App">
         <h1>Redux Learn</h1>
         <p>Count2: {count}</p>
+        <button onClick={increase}>Up</button>
+        <button onClick={decrease}>Down</button>
         <ul>
           {posts.map((post) => (
             <li key={post.id}>{post.title}</li>
