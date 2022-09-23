cd698a001c0223108d2dc3c4a1747d0191252095
agiledots <agiledots@gmail.com>
2022-09-23 11:09:35 +0800

    feat: reduxを導入した。

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import "./App.css";
 
+import store from "./store/index";
+
 function App() {
   return (
     <div>
-      <h1>Redux Learn</h1>
+      <div className="App">
+        <h1>Redux Learn</h1>
+        <p>Count:{store.getState().count}</p>
+      </div>
     </div>
   );
 }
