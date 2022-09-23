00aad08a4bbe222a97fc000648efb3e2fc7cf1e9
agiledots <agiledots@gmail.com>
2022-09-23 11:37:17 +0800

    feat: actionを追加した。

--- src/components/Count2.js
+++ src/components/Count2.js
@@ -3,12 +3,22 @@ import { connect } from "react-redux";
 
 // Componentsは mapStateToProps で redux の state を取得
 
-function Count2({ count, posts }) {
+function Count2({ dispatch, count, posts }) {
+  const increase = () => {
+    dispatch({ type: "INCREASE_COUNT" });
+  };
+  const decrease = () => {
+    dispatch({ type: "DECREASE_COUNT" });
+  };
+
   return (
     <div>
       <div className="App">
         <h1>Redux Learn</h1>
         <p>Count2: {count}</p>
+        <button onClick={increase}>Up</button>
+        <button onClick={decrease}>Down</button>
+
         <ul>
           {posts.map((post) => (
             <li key={post.id}>{post.title}</li>
--- src/store/reducers/countReducer.js
+++ src/store/reducers/countReducer.js
@@ -1,7 +1,18 @@
-export const countReducer = (
-  state = {
-    count: 50,
+const initialState = {
+  count: 50,
+};
+
+export const countReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "INCREASE_COUNT":
+      return {
+        count: state.count + 1,
+      };
+    case "DECREASE_COUNT":
+      return {
+        count: state.count - 1,
+      };
+    default:
+      return state;
   }
-) => {
-  return state;
 };
